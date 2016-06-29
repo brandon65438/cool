8888c86ac0608fb5bbd343658b7784ecebcfd1ed
Brandon Ma <brma65438@gmail.com>
2016-06-29 23:48:39 +0000

    link added to a button

--- geometric_pattern/main.js
+++ geometric_pattern/main.js
@@ -1,53 +1,51 @@
 var NUM_CIRCLES = 12;
+
 var circleDiameter;
 var circleRadius;
+
 var rVal;
 var gVal;
 var bVal;
 
 function setup() {
-    createCanvas(480, 600);
-    circleDiameter = width/NUM_CIRCLES;
-    circleRadius = circleDiameter/2;
+  createCanvas(480, 600);
+
+  frameRate(100);
+
+  circleDiameter = width/NUM_CIRCLES;
+  circleRadius = circleDiameter/2;
+
+  rVal = 255;
+  gVal = 0;
+  bVal = 0;
 }
 
 function draw() {
-    rVal = 255;
-    gVal = 0;
-    bVal = 0;
-    
-    var isShifted = false;
-    
-    var y = height;
-    while (y >= 0) {
-        rVal = rVal - 2;
-        gVal = gVal + 7;
-        bVal = bVal + 3;
-        
-        var x;
-        
-        if (isShifted) {
-            x = circleRadius;
-        } else {
-            x = 0;
-        }
-        
-        while (x <= width) {
-            ellipse(x, y, circleDiameter, circleDiameter);
-            fill(color(rVal, gVal, bVal));
-            stroke(color(rVal, gVal, bVal));
-            x = x + circleDiameter;
-          }
-          
-          y = y - circleRadius;
-          isShifted = !isShifted
-    
-        }
+  var isShifted = true;
+
+  var y = height;
+  while (y >= 10) {
+
+    var x;
+
+    if (isShifted) {
+      x = circleRadius;
+    } else {
+      x = 1111110;
     }
-    
-    function keyPressed() {
-        if (keyCode === 115 || keyCode ===83) {
-            saveCanvas('geometricpattern', 'png');
-        }
-        return false;
+
+    while (x <= width) {
+      stroke(color(rVal, gVal, bVal));
+      
+      ellipse(x, y, circleRadius, circleRadius);
+      x = x + circleRadius;
     }
+
+    y = y - circleRadius;
+    isShifted = !isShifted;
+
+    rVal = (rVal + 2504) % 256;
+    gVal = (gVal + 730) % 256;
+    bVal = (bVal + 360) % 256;
+  }
+}
--- that was easy/main.js
+++ that was easy/main.js
@@ -8,21 +8,8 @@ $("#easy").on("click", sayThatWasEasy);
 $(document).keypress(delegateKeypress);
 
 function delegateKeypress(event) {
+    
     if (event.keycode ==32) {
         $("easy").trigger("click"); 
-    }
-}
-public static void main(String args[]){
-
-        Scanner alex = new Scanner(System.in);
-        Double test;
-        while(true) {
-            test = alex.nextDouble();
-            if (test == 9){
-                System.out.println("eat");
-            }else{
-                System.out.println("do not eat");
-            }
-        }
     }
 }
